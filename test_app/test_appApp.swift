6488ff0c23ad80ba0722d81b74dcd97a299a6b10
Mohamad Khajezade <12826803+mkhfring@users.noreply.github.com>
2026-01-12 01:19:28 -0800

    Initial Commit

---
+++ test_app/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  test_app
+//
+//  Created by MK on 2026-01-12.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
